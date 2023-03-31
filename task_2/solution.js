8ff90e40f37d59a989323c59a6744a5b0c2d69b5
A.Illarionov <icostra@gmail.com>
2023-03-31 22:58:58 +0500

    change solution.js task2

--- task_2/solution.js
+++ task_2/solution.js
@@ -17,7 +17,7 @@ function calcShipping(sum, min, shipping) {
     
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    if (productsSum >0 <freeShippingMinSum){
+    if ((productsSum >0) && (productsSum <freeShippingMinSum)){
         shippingSum = shippingPrice;
     }
     // Конец решения задания №2.1.
