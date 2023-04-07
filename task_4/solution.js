03ea37a4984a3106558a617921881262e21ffbff
A.Illarionov <icostra@gmail.com>
2023-04-07 08:32:43 +0500

    change solution.js task 4

--- task_4/solution.js
+++ task_4/solution.js
@@ -4,7 +4,7 @@ function parseProducts(json) {
     // Аргументом функции является JSON
     let data = JSON.parse(json);
     let products = data.products;
-    return products
+    return products;
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
